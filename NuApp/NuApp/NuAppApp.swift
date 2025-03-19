dcf1e5d3e1fb75743fc898b8671d410a923b3a77
Arthur Rodrigues <arthursmr17@gmail.com>
2025-03-19 19:33:38 -0300

    feat: initial project setup

---
+++ NuApp/NuApp/NuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuAppApp.swift
+//  NuApp
+//
+//  Created by Arthur Rodrigues on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
